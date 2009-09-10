46b1887b6d7456085fa305160c43a71f39c58391
Bernardo Roubach <broubach@yahoo.com.br>
2009-09-10 21:31:46 +0000

    Aplicação do novo visual
    git-svn-id: svn://localhost/barragem/trunk@55 a01e8a6f-d01e-c94b-8b3f-b6c26fcad43f

--- WebContent/js/barragem.js
+++ WebContent/js/barragem.js
@@ -1,5 +1,6 @@
-	function aplicarMascaras() {
-		$j('.date').mask("99/99/9999"); 
-		$j('.dateTime').mask("99/99/9999 99:99"); 
-		$j('.parcial').mask("9");
-	}
+$(function(){
+	$('.language li').not('.active').fadeOut();
+	$('.language li').click(function(){
+	    $(this).siblings().toggle('slow');
+	})	
+})
